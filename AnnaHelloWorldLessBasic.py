5922b6938f71f3e43fd1755de9cd9d46803a6d16
harveya79 <148778048+harveya79@users.noreply.github.com>
2023-10-24 14:13:23 +0000

    Added a slightly more challenging python code file
    It now prints a personalised message

---
+++ AnnaHelloWorldLessBasic.py
@@ -0,0 +1,6 @@
+# This is a simple python file
+# It displays a simple message 'Hello World' to the terminal/command line
+print("Hello World!")
+
+# Something slightly more interesting 
+print("Hello Anna!")
